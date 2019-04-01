a9f7a6d25b2d93fd09e4d20b63b0df77293d160e
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-04-01 14:55:56 +0700

    Add background

--- strike.py
+++ strike.py
@@ -2,19 +2,36 @@ import arcade
  
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
- 
-class MazeWindow(arcade.Window):
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
+
+class StrikeWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.PINK_PEARL)
+        self.background = arcade.load_texture("images/background.png")
  
     def on_draw(self):
         arcade.start_render()
- 
- 
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)     
+
+
 def main():
-    window = MazeWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window = StrikeWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.set_window(window)
     arcade.run()
  
